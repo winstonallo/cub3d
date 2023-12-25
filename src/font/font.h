56a05c387f5fb30a1235a5a7cf93c6224f4b18bb
chair <yannis@gmx.at>
2023-12-26 00:19:47 +0100

    random commit

--- src/font/font.h
+++ src/font/font.h
@@ -13,12 +13,12 @@
 #ifndef FONT_H
 # define FONT_H
 
-# include <mlx.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include "../../../libft/libft.h"
+# include "../../minilibx-linux/mlx.h"
 
 typedef struct s_img
 {
