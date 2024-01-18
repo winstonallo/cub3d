bdf8c7c54cb88460c30275f9fd0611f4ae9bddb3
winstonallo <arthurbiedchar@gmail.com>
2024-01-18 17:33:18 +0100

    its getting somwheher

--- src/raycaster/raycasting_utils.c
+++ src/raycaster/raycasting_utils.c
@@ -6,7 +6,7 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 12:34:50 by abied-ch          #+#    #+#             */
-/*   Updated: 2024/01/18 17:18:57 by abied-ch         ###   ########.fr       */
+/*   Updated: 2024/01/18 17:32:42 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,6 @@ void	adjust_vars(t_data *data, float angle)
 		- (data->line_height * WALL_HEIGHT);
 }
 
-void	get_hit_direction(t_line line, t_data *data)
-{
-	if (line.direction == VERTICAL)
-	{
-		if (line.y_inc > 0)
-			data->hit = EAST;
-		else
-			data->hit = WEST;
-	}
-	else
-	{
-		if (line.x_inc > 0)
-			data->hit = SOUTH;
-		else
-			data->hit = NORTH;
-	}
-}
-
 void	calculate_distance(t_data *data, t_line line1, t_line line2)
 {
 	float	min_dist1;
@@ -49,8 +31,14 @@ void	calculate_distance(t_data *data, t_line line1, t_line line2)
 	min_dist1 = dist(line1);
 	min_dist2 = dist(line2);
 	if (min_dist1 < min_dist2)
+	{
 		set_shortest_line_vars(data, min_dist1, line1, line1.y1);
+		data->hit = (data->shortest_line.x1 - data->shortest_line.x0) >= 0 ? SOUTH : NORTH;
+	}
 	else
+	{
 		set_shortest_line_vars(data, min_dist2, line2, line2.x1);
-	get_hit_direction(data->shortest_line, data);
+		data->hit = (data->shortest_line.y1 - data->shortest_line.y0) >= 0 ? EAST : WEST;
+	}
+	// get_hit_direction(data->shortest_line, data);
 }
